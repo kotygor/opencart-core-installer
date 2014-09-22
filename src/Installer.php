929603ebbcd89f1af09ad130ef8f0add8275a30c
Fike Etki <etki@etki.name>
2014-09-22 05:16:25 +0400

    Implemented file rotating

--- src/Installer.php
+++ src/Installer.php
@@ -3,9 +3,12 @@ namespace Etki\Composer\Installers\Opencart;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Repository\InstalledRepositoryInterface;
 
 /**
  * Just an opencart installer, nothing to see here, move along.
+ *
+ * @todo implement caching for calculated install dirs
  *
  * @version 0.1.0
  * @since   0.1.0
@@ -54,6 +57,7 @@ class Installer extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
+        $prettyName = $package->getPrettyName();
         $installDir = null;
         if ($this->composer->getPackage()) {
             $rootExtra = $this->composer->getPackage()->getExtra();
@@ -68,12 +72,65 @@ class Installer extends LibraryInstaller
             }
         }
         if (is_array($installDir)) {
-            $prettyName = $package->getPrettyName();
             if (isset($installDir[$prettyName])) {
                 return $installDir[$prettyName];
             }
             return $this->defaultInstallDir;
         }
         return $installDir ? $installDir : $this->defaultInstallDir;
+    }
+
+    /**
+     * Moves opencart from it's basic `upload` dir one level higher.
+     *
+     * @param string $installPath Path to files.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    protected function rotateFiles($installPath)
+    {
+        $parentDir = dirname($installPath);
+        $tempDir = $parentDir . DIRECTORY_SEPARATOR . 'tmp-oi-' . md5(time());
+        $uploadDir = $tempDir . DIRECTORY_SEPARATOR . 'upload';
+        $this->filesystem->rename($installPath, $tempDir);
+        $this->filesystem->rename($uploadDir, $installPath);
+        $this->filesystem->remove($tempDir);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param InstalledRepositoryInterface $repo    Repository,
+     * @param PackageInterface             $package Package.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function install(
+        InstalledRepositoryInterface $repo,
+        PackageInterface $package
+    ) {
+        parent::install($repo, $package);
+        $this->rotateFiles($this->getInstallPath($package));
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param InstalledRepositoryInterface $repo    Repository,
+     * @param PackageInterface             $initial Current package.
+     * @param PackageInterface             $target  Newly installed package.
+     *
+     * @return void
+     * @since 0.1.0
+     */
+    public function update(
+        InstalledRepositoryInterface $repo,
+        PackageInterface $initial,
+        PackageInterface $target
+    ) {
+        parent::update($repo, $initial, $target);
+        $this->rotateFiles($this->getInstallPath($target));
     }
 }
