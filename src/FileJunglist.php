09399dd6afe0c6dcac3e3e3ec99ba55806046da8
JD <zelenuj@gmail.com>
2020-10-22 14:20:16 +0300

    Процес зміни організації файлів

--- src/FileJunglist.php
+++ src/FileJunglist.php
@@ -206,7 +206,7 @@ class FileJunglist
 	    }
 	    else {
 //	    	$files = glob($projectRootFolder . DIRECTORY_SEPARATOR . '*');
-		    $fsm->rename($webRootFolder, $tempDir);
+		    $fsm->rename($projectRootFolder, $tempDir);
 		    $files = glob($tempDir . DIRECTORY_SEPARATOR . '*');
 	    	DebugPrinter::log("projectRootFiles: %s", print_r($files, 1));
 
